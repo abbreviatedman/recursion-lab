87b50f204f666dffe9298cde31aff3ded0921b81
Colin Jaffe <balloonasaurus@gmail.com>
2021-04-09 13:09:38 -0400

    Adds numbers as a We Do.

---
+++ numbers.py
@@ -0,0 +1,42 @@
+print("### add_up_to")
+def add_up_to (n):
+  pass
+
+result1 = add_up_to(0)
+print(f"result1 should be 0 and is, in fact: {result1}")
+
+result2 = add_up_to(1)
+print(f"result2 should be 1 and is, in fact: {result2}")
+
+result3 = add_up_to(3)
+# 3 + 2 + 1 + 0
+print(f"result3 should be 6, in fact: {result3}")
+
+result4 = add_up_to(4)
+# 4 + 3 + 2 + 1 + 0
+print(f"result4 should be 10, in fact: {result4}")
+
+result5 = add_up_to(10)
+# 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + 0
+print(f"result5 should be 55, in fact: {result5}")
+
+print("### factorial")
+def factorial (n):
+  pass
+
+result1 = factorial(0)
+print(f"result1 should be 0 and is, in fact: {result1}")
+
+result2 = factorial(1)
+print(f"result2 should be 1 and is, in fact: {result2}")
+
+result3 = factorial(2)
+print(f"result3 should be 2 and is, in fact: {result3}")
+
+result4 = factorial(3)
+# 3 * 2 * 1
+print(f"result4 should be 6 and is, in fact: {result4}")
+
+result5 = factorial(6)
+#6 * 5 * 4 * 3 * 2 * 1
+print(f"result5 should be 720, and is, in fact: {result5}")
